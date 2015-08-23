2d485a401ac4f3a82827b2d3de7eed21a9990a71
Kenneth Endfinger <kaendfinger@gmail.com>
2015-08-23 14:15:13 -0400

    Add Support for Data Client Servers

--- bin/run.dart
+++ bin/run.dart
@@ -10,10 +10,9 @@ import "package:http_multi_server/http_multi_server.dart";
 LinkProvider link;
 
 JsonEncoder jsonEncoder = new JsonEncoder.withIndent("  ");
-
 String toJSON(input) => jsonEncoder.convert(input);
 
-launchServer(bool local, int port, SimpleNode node) async {
+launchServer(bool local, int port, ServerNode serverNode) async {
   List<HttpServer> servers = <HttpServer>[];
   InternetAddress ipv4 = local ? InternetAddress.LOOPBACK_IP_V4 : InternetAddress.ANY_IP_V4;
   InternetAddress ipv6 = local ? InternetAddress.LOOPBACK_IP_V6 : InternetAddress.ANY_IP_V6;
@@ -34,9 +33,9 @@ launchServer(bool local, int port, SimpleNode node) async {
     Uri uri = request.uri;
     String method = request.method;
     String ourPath = Uri.decodeComponent(uri.normalizePath().path);
-    String path = "${node.path}${ourPath}";
-    if (path.endsWith("/")) {
-      path = path.substring(0, path.length - 1);
+    String hostPath = "${serverNode.path}${ourPath}";
+    if (hostPath.endsWith("/")) {
+      hostPath = hostPath.substring(0, hostPath.length - 1);
     }
 
     response.headers.set("Cache-Control", "no-cache, no-store, must-revalidate");
@@ -51,7 +50,42 @@ launchServer(bool local, int port, SimpleNode node) async {
       return;
     }
 
-    Path p = new Path(path);
+    Path p = new Path(hostPath);
+
+    Future<Map> getRemoteNodeMap(RemoteNode n) async {
+      if (n == null) {
+        return {
+          "error": "No Such Node"
+        };
+      }
+
+      var p = new Path(n.remotePath);
+      var map = {
+        "?name": p.name,
+        "?path": ourPath
+      };
+
+      map.addAll(n.configs);
+      map.addAll(n.attributes);
+
+      for (String key in n.children.keys) {
+        RemoteNode child = n.children[key];
+
+        var x = new Path(child.remotePath);
+        map[key] = {
+          "?name": x.name,
+          "?path": (ourPath == "/" ? "" : ourPath) + "/" + key
+        }..addAll(child.getSimpleMap());
+      }
+
+      if (n.configs.containsKey(r"$type")) {
+        var val = await link.requester.getNodeValue(ourPath);
+        map["?value"] = val.value;
+        map["?value_timestamp"] = val.ts;
+      }
+
+      return map;
+    }
 
     Map getNodeMap(SimpleNode n) {
       if (n == null) {
@@ -69,7 +103,7 @@ launchServer(bool local, int port, SimpleNode node) async {
       var p = new Path(n.path);
       var map = {
         "?name": p.name,
-        "?path": "/" + path.split("/").skip(2).join("/")
+        "?path": "/" + hostPath.split("/").skip(2).join("/")
       };
 
       map.addAll(n.configs);
@@ -98,9 +132,17 @@ launchServer(bool local, int port, SimpleNode node) async {
     }
 
     if (method == "GET") {
-      SimpleNode n = link.getNode(path);
+      if (!serverNode.isDataHost) {
+        var node = await link.requester.getRemoteNode(ourPath);
+        response.headers.contentType = ContentType.JSON;
+        response.writeln(toJSON(await getRemoteNodeMap(node)));
+        response.close();
+        return;
+      }
 
-      if (link.provider.getNode(path) == null) {
+      SimpleNode n = link.getNode(hostPath);
+
+      if (link.provider.getNode(hostPath) == null) {
         response.statusCode = HttpStatus.NOT_FOUND;
         response.headers.contentType = ContentType.JSON;
         response.writeln(toJSON({
@@ -117,6 +159,16 @@ launchServer(bool local, int port, SimpleNode node) async {
       response.close();
       return;
     } else if (method == "PUT") {
+      if (!serverNode.isDataHost) {
+        response.statusCode = HttpStatus.NOT_IMPLEMENTED;
+        response.headers.contentType = ContentType.JSON;
+        response.writeln(toJSON({
+          "error": "Data Client does not support creating/updating nodes"
+        }));
+        response.close();
+        return;
+      }
+
       var json = await readJSONData(request);
       var mp = p.parent;
       var pathsToCreate = [];
@@ -134,8 +186,8 @@ launchServer(bool local, int port, SimpleNode node) async {
         }
       }
 
-      if (link.provider.getNode(path) != null) {
-        var node = link.provider.getNode(path);
+      if (link.provider.getNode(hostPath) != null) {
+        var node = link.provider.getNode(hostPath);
         Map map;
         if (json.keys.length == 1 && json.keys.contains("?value")) {
           node.updateValue(new ValueUpdate(json["?value"], ts: ValueUpdate.getTs()));
@@ -154,7 +206,7 @@ launchServer(bool local, int port, SimpleNode node) async {
         return;
       }
 
-      var node = link.addNode(path, json);
+      var node = link.addNode(hostPath, json);
       var map = getNodeMap(node);
       map[r"$is"] = "rest";
       response.headers.contentType = ContentType.JSON;
@@ -163,11 +215,36 @@ launchServer(bool local, int port, SimpleNode node) async {
       changed = true;
       return;
     } else if (method == "POST" || method == "PATCH") {
-      SimpleNode node = link.getNode(path);
       var json = await readJSONData(request);
 
+      if (!serverNode.isDataHost) {
+        if (json.keys.length == 1 && json.keys.contains("?value")) {
+          var val = json["?value"];
+          await link.requester.set(ourPath, val);
+          response.headers.contentType = ContentType.JSON;
+          response.writeln(toJSON(await getRemoteNodeMap(await link.requester.getRemoteNode(ourPath))));
+          response.close();
+        } else if (uri.queryParameters.containsKey("val") || uri.queryParameters.containsKey("value")) {
+          await link.requester.set(ourPath, json);
+          response.headers.contentType = ContentType.JSON;
+          response.writeln(toJSON(await getRemoteNodeMap(await link.requester.getRemoteNode(ourPath))));
+          response.close();
+          return;
+        } else {
+          response.statusCode = HttpStatus.NOT_IMPLEMENTED;
+          response.headers.contentType = ContentType.JSON;
+          response.writeln(toJSON({
+            "error": "Data Client does not support updating nodes"
+          }));
+          response.close();
+          return;
+        }
+      }
+
+      SimpleNode node = link.getNode(hostPath);
+
       if (node == null) {
-        node = link.addNode(path, json);
+        node = link.addNode(hostPath, json);
         var map = getNodeMap(node);
         response.headers.contentType = ContentType.JSON;
         response.writeln(toJSON(map));
@@ -180,7 +257,7 @@ launchServer(bool local, int port, SimpleNode node) async {
       if (json.keys.length == 1 && json.keys.contains("?value")) {
         node.updateValue(json["?value"]);
         map = getNodeMap(node);
-      } else if (uri.queryParameters.containsKey("val")) {
+      } else if (uri.queryParameters.containsKey("val") || uri.queryParameters.containsKey("value")) {
         node.updateValue(json);
         map = getNodeMap(node);
       } else {
@@ -195,7 +272,16 @@ launchServer(bool local, int port, SimpleNode node) async {
       changed = true;
       return;
     } else if (method == "DELETE") {
-      SimpleNode node = link.getNode(path);
+      if (!serverNode.isDataHost) {
+        response.statusCode = HttpStatus.NOT_IMPLEMENTED;
+        response.headers.contentType = ContentType.JSON;
+        response.writeln(toJSON({
+          "error": "Data Clients do not support DELETE"
+        }));
+        response.close();
+      }
+
+      SimpleNode node = link.getNode(hostPath);
       var map = getNodeMap(node);
 
       if (node == null) {
@@ -250,6 +336,7 @@ main(List<String> args) async {
     "addServer": (String path) => new SimpleActionNode(path, (Map<String, dynamic> params) async {
       int port = params["port"] is String  ? int.parse(params["port"]) : params["port"];
       bool local = params["local"];
+      String type = params["type"];
       if (local == null) local = false;
 
       try {
@@ -265,6 +352,7 @@ main(List<String> args) async {
         r"$is": "server",
         r"$server_port": port,
         r"$server_local": local,
+        r"$server_type": type,
         "Remove": {
           r"$is": "remove",
           r"$invokable": "write"
@@ -313,7 +401,7 @@ main(List<String> args) async {
       });
     },
     "remove": (String path) => new DeleteActionNode.forParent(path, link.provider)
-  }, autoInitialize: false);
+  }, autoInitialize: false, isRequester: true, isResponder: true);
 
   var nodes = {
     "Add_Server": {
@@ -335,6 +423,12 @@ main(List<String> args) async {
           "name": "port",
           "type": "int",
           "default": 8020
+        },
+        {
+          "name": "type",
+          "type": "enum[Data Host,Data Client]",
+          "default": "Data Host",
+          "description": "Data Type"
         }
       ],
       r"$result": "values",
@@ -470,24 +564,34 @@ class ServerNode extends SimpleNode {
   onCreated() async {
     var port = configs[r"$server_port"];
     var local = configs[r"$server_local"];
+    var type = configs[r"$server_type"];
     if (local == null) local = false;
+    if (type == null) type = "Data Host";
+
+    configs[r"$server_local"] = local;
+    configs[r"$server_type"] = type;
+
     server = await launchServer(local, port, this);
 
-    link.addNode("${path}/Create_Node", {
-      r"$name": "Create Node",
-      r"$is": "create",
-      r"$invokable": "write",
-      r"$result": "values",
-      r"$params": [
-        {
-          "name": "name",
-          "type": "string"
-        }
-      ]
-    });
+    if (type == "Data Host") {
+      link.addNode("${path}/Create_Node", {
+        r"$name": "Create Node",
+        r"$is": "create",
+        r"$invokable": "write",
+        r"$result": "values",
+        r"$params": [
+          {
+            "name": "name",
+            "type": "string"
+          }
+        ]
+      });
 
-    link.addNode("${path}/Create_Value", CREATE_VALUE);
+      link.addNode("${path}/Create_Value", CREATE_VALUE);
+    }
   }
+
+  bool get isDataHost => configs[r"$server_type"] == "Data Host";
 
   @override
   onRemoving() async {
