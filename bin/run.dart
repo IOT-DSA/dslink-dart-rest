8479fef6df06d20e38bb364e54fde97224f4d883
Matthew Butler <butler.matthew@gmail.com>
2017-03-23 09:09:56 -0300

    Dartfmt run as well

--- bin/run.dart
+++ bin/run.dart
@@ -6,21 +6,20 @@ import 'package:dslink_rest/rest.dart';
 
 Future<Null> main(List<String> args) async {
   LinkProvider link;
-  link = new LinkProvider(args, "REST-", profiles: {
-      AddServer.isType: (String path) => new AddServer(path, link),
-      ServerNode.isType: (String path) => new ServerNode(path, link),
-      CreateValue.isType: (String path) => new CreateValue(path, link),
-      CreateNode.isType: (String path) => new CreateNode(path, link),
-      RemoveNode.isType: (String path) => new RemoveNode(path, link),
-      RestNode.isType: (String path) => new RestNode(path),
-    }, defaultNodes: {
-      AddServer.pathName: AddServer.def()
-    },
-    autoInitialize: false, isRequester: true, isResponder: true);
+  link = new LinkProvider(args, "REST-",
+      profiles: {
+        AddServer.isType: (String path) => new AddServer(path, link),
+        ServerNode.isType: (String path) => new ServerNode(path, link),
+        CreateValue.isType: (String path) => new CreateValue(path, link),
+        CreateNode.isType: (String path) => new CreateNode(path, link),
+        RemoveNode.isType: (String path) => new RemoveNode(path, link),
+        RestNode.isType: (String path) => new RestNode(path),
+      },
+      defaultNodes: {AddServer.pathName: AddServer.def()},
+      autoInitialize: false,
+      isRequester: true,
+      isResponder: true);
 
   link.init();
   await link.connect();
-
 }
-
-
